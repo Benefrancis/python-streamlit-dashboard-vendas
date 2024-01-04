d1bc72cacf6b6dcd9143bbd1e70c0d63bbba376f
Benefrancis <benefrancis@gmail.com>
2024-01-04 02:49:36 -0300

    requirements.txt

--- Dashboard.py
+++ Dashboard.py
@@ -27,12 +27,38 @@ DeltaGenerator()
 ## Requisição para a API
 
 url = 'https://labdados.com/produtos'
-response = requests.get(url)
+
+regioes = ['Brasil', 'Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul']
+
+st.sidebar.title('Filtros')
+regiao = st.sidebar.selectbox('Região', regioes)
+
+if regiao == 'Brasil':
+    regiao = ''
+
+todos_anos = st.sidebar.checkbox('Dados de todo o período', value=True)
+
+if todos_anos:
+    ano = ''
+else:
+    ano = st.sidebar.slider('Ano', 2020, 2023)
+
+query_string = {'regiao': regiao.lower(), 'ano': ano}
+response = requests.get(url, params=query_string)
+
 dados = pd.DataFrame.from_dict(response.json())
 
 # %%
 ## Alterando formato da data
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/%Y')
+
+# %%
+## Filtro de vendedores
+
+filtro_vendedores = st.sidebar.multiselect('Vendedores', dados['Vendedor'].unique())
+
+if filtro_vendedores:
+    dados = dados[dados['Vendedor'].isin(filtro_vendedores)]
 
 # %%
 ## Receita por UF
